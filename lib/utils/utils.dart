78fa17c234e8825de80445582bb3fa5e87a8eaed
austinmedina <austinmedina@comcast.net>
2025-01-04 21:46:30 -0800

    Removed unecessary code from BooleanWrapper

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,34 +1,4 @@
-import 'package:flutter/foundation.dart'; // Import for Listenable
-
-class BooleanWrapper with ChangeNotifier implements Listenable{
-  bool _value;
-
-  BooleanWrapper(this._value);
-
-  bool get value => _value;
-
-  set value(bool newValue) {
-    if (_value != newValue) {
-      _value = newValue;
-      notifyListeners(); // Notify listeners when the value changes
-    }
-  }
-}
-
-class BooleanWrapperValueNotifier extends ValueNotifier<bool> {
-  final BooleanWrapper _booleanWrapper;
-
-  BooleanWrapperValueNotifier(this._booleanWrapper) : super(_booleanWrapper.value) {
-    _booleanWrapper.addListener(_updateValue);
-  }
-
-  void _updateValue() {
-    value = _booleanWrapper.value;
-  }
-
-  @override
-  void dispose() {
-    _booleanWrapper.removeListener(_updateValue);
-    super.dispose();
-  }
+class BooleanWrapper {
+  bool value;
+  BooleanWrapper(this.value);
 }
